747cb4b49d91b8471c0eafd8f304a1e64d3dcda8
Prem Brahmbhatt <81856256+theprembrahmbhatt@users.noreply.github.com>
2024-10-29 17:47:32 -0700

    Create SalesReducer.java

---
+++ SalesReducer.java
@@ -0,0 +1,18 @@
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
+
+public class SalesReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> {
+    private FloatWritable totalSales = new FloatWritable();
+
+    @Override
+    protected void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException {
+        float sum = 0;
+        for (FloatWritable value : values) {
+            sum += value.get();
+        }
+        totalSales.set(sum);
+        context.write(key, totalSales);
+    }
+}
